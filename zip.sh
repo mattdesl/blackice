0e1068707253136bbc8b28bcf85214a04ebb0128
mattdesl <dave.des@gmail.com>
2015-02-03 23:11:31 -0500

    updating build

---
+++ zip.sh
@@ -0,0 +1,14 @@
+rm -f zip/blackice-demo.zip && \
+zip zip/blackice-demo.zip -r \
+        build/three-libs.min.js \
+        build/blackice.min.js \
+        fonts/Exo2SemiBold.fnt \
+        fonts/Exo2SemiBold.png \
+        img/lookup.png \
+        img/dust.jpg \
+        img/waternormals5.jpg \
+        img/cloudy_night2.jpg \
+        model/Prirazlomnayawithpipes3.mtl \
+        model/Prirazlomnayawithpipes3.obj \
+        model/texture.png \
+        test.html
